ff672ebcc160bc69b05c4f451c08613fafea2e07
Oleksandr274 <s00274217@atu.ie>
2026-03-28 23:17:12 +0000

    backend: add product data to the stripe checkout

--- restapiinexpress-Oleksandr274/src/controllers/checkout.ts
+++ restapiinexpress-Oleksandr274/src/controllers/checkout.ts
@@ -3,23 +3,45 @@ import dotenv from "dotenv";
 dotenv.config();
 import { stripe } from '../models/stripe';
 import { Item } from '../models/item';
+import { ObjectId } from 'mongodb';
+import { collections } from '../database';
+import { Product } from '../models/products';
 
 const PORT = process.env.PORT || 3001;
-const DOMAIN = `http://localhost:${PORT}`;
 
 export const createCheckoutSession = async (req: Request, res: Response) => {
   try {
     const items = req.body.items as Item[]
+    let productsList: any[] = [];
+    // find products from items list
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      if (ObjectId.isValid(item.productId)) {
+        const product = (await collections.products?.findOne({ _id: new ObjectId(item.productId) })) as unknown as Product;
+
+        if (product) {
+          productsList.push(product);
+        } else {
+          res.status(400).send('Unable to create Checkout (product does not exist)');
+          return;
+        }
+      } else {
+        res.status(400).send('Unable to create Checkout (product does not exist)');
+        return;
+      }
+    }
+
+    console.log(productsList.find(p=>p._id == items[0].productId));
 
     const session = await stripe.checkout.sessions.create({
       line_items: items.map((item: Item) => ({
         price_data: {
           currency: 'usd',
           product_data: {
-            name: item.productId.toString(),
-            images: ['https://placehold.co/600x400/EEE/31343C']
+            name: productsList.find(p =>p._id == item.productId).title ,
+            images: [productsList.find(p =>p._id == item.productId).images[0]]
           },
-          unit_amount: 100 * 100, //cents
+          unit_amount: productsList.find(p =>p._id == item.productId).price * 100, //cents
         },
         quantity: item.quantity
       })),
