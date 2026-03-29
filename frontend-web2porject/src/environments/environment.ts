b7b1775bc7a01048fad5bba6d13a3154a538c9da
Oleksandr274 <s00274217@atu.ie>
2026-03-30 00:21:30 +0100

    fix end vars

--- frontend-web2porject/src/environments/environment.ts
+++ frontend-web2porject/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
      production: true,
-     apiUri: 'https://osd-project-2026-oleksandr274.onrender.com/api/v1/',
+     apiUri: 'https://osd-project-2026-oleksandr274.onrender.com/api/v1',
      lambdaUri: 'https://ecslp12cfb.execute-api.eu-west-1.amazonaws.com/dev'
   };
 
