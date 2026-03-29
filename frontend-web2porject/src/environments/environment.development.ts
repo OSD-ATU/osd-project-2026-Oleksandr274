06ff086fb1ec07f7db4271a3295c911b77247933
Oleksandr274 <s00274217@atu.ie>
2026-03-30 00:14:32 +0100

    сhamge env variables

--- frontend-web2porject/src/environments/environment.development.ts
+++ frontend-web2porject/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUri: 'http://localhost:3000/api/v1',
+  apiUri: 'https://osd-project-2026-oleksandr274.onrender.com/api/v1/',
   lambdaUri: 'https://ecslp12cfb.execute-api.eu-west-1.amazonaws.com/dev'
 };
